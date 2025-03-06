6d78b0ca2c87098dea3c9c837958eb4b3828e9b9
Smungtek <mengteck.tmt@gmail.com>
2025-03-07 01:54:32 +0800

    Reworked base object to reference bytecode instead

--- AdvancedPrimitives/advanced_primitives.h
+++ AdvancedPrimitives/advanced_primitives.h
@@ -1,17 +1,42 @@
 #ifndef ADVANCED_PRIMITIVES
 #define ADVANCED_PRIMITIVES
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/* foward delcaration */
-typedef struct objectBase objectBase;
+/* Forward declaration */
 typedef struct Object Object;
 
-/* Function pointer type for "dunder methods" */
-typedef Object * (*dunder_operator) (Object*, Object*);
-typedef Object * (*dunder_method) (Object*);
+/* Structure for a function reference (points to bytecode in memory) */
+typedef struct {
+    char *name;       // Method name
+    size_t bytecode_offset; // Bytecode location in memory/file
+} MethodReference;
 
-/* baseObject */
+/* Structure for storing method references */
+typedef struct {
+    MethodReference **entries; // Array of method references
+    size_t size;
+    size_t capacity;
+} MethodTable;
 
+/* Object Base */
+typedef struct {
+    char *type;
+} objectBase;
 
+/* Object Structure */
+struct Object {
+    objectBase base;
+    Object *parent;
+    MethodTable *methods; // holds the method reference
+};
 
-#endif
+/* Function prototypes */
+Object *Object_new(const char *type, Object *parent);
+void Object_add_method(Object *obj, const char *name, size_t bytecode_offset);
+size_t Object_get_method(Object *obj, const char *name);
+void Object_destroy(Object *obj);
+
+#endif /* ADVANCED_PRIMITIVES */
